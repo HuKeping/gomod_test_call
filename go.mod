95ce4eedfb0652b4d302d6b4335687a0a7c7382f
Hu Keping <hukeping@huawei.com>
2021-01-05 20:46:18 +0800

    init commit
    Signed-off-by: Hu Keping <hukeping@huawei.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/HuKeping/gomod_test_call
+
+go 1.13
+
+require github.com/HuKeping/gomod_test v3.0.0+incompatible
---
+++ main.go
@@ -0,0 +1,8 @@
+package gomod_test_call
+
+import "github.com/HuKeping/gomod_test/v3_dir"
+
+
+func Gomod_test_call() {
+	v3_dir.Gomod_test_v3()
+}
